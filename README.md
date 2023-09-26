3baac17ffc01f8b50e95689870a7385b5360dcc1
hyundok <gmcc0713@naver.com>
2023-09-26 14:26:08 +0900

    코딩스타일 추가(ReadMe)
    코딩스타일 추가(ReadMe)

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 TeamByte
 
 
-
-gdgdgdd
+//코딩 스타일
+멤버변수 -> m_타입이름 -> m_iDamage
+매개변수 -> _damage
+지역변수 -> damage
+get set 함수? -> _damage
---
+++ TeamByte/Assets/_Script/Monster.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MonsterData
+{
+   private int damage;
+    public int _damage => damage;
+}
+public class Monster : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
